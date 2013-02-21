c1ad95743d67974436cbcfeebfeda71da54af143
Guillaume Leroi <guillaume.leroi@keyconsulting.fr>
2013-02-21 11:06:20 +0100

    add a base page for razor view

---
+++ Vernacular.Mvc/VernacularWebViewPage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Vernacular.Mvc
+{
+    public abstract class VernacularWebViewPage<T> : WebViewPage<T>
+    {
+
+    }
+}
