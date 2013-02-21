5df513d095de36021684675e832f6decfbbd0203
Guillaume Leroi <guillaume.leroi@keyconsulting.fr>
2013-02-21 10:43:32 +0100

    Add script and specs for creating a nuget package

--- Vernacular.Tool/Properties/AssemblyInfo.cs
+++ Vernacular.Tool/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Vernacular")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A localization tool for all")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Rdio, Inc")]
 [assembly: AssemblyProduct("Vernacular")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
---
+++ make-nuget.cmd
@@ -0,0 +1,1 @@
+nuget pack Vernacular.Tool/Vernacular.Tool.csproj
