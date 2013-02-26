6ff90755a530c1aa76ba23fb6981fc1705814ac4
Guillaume Leroi <guillaume.leroi@keyconsulting.fr>
2013-02-26 09:29:55 +0100

    VernacularConfig inserts itself in httpapplication, limits user to a call in Application_Start

--- Vernacular.Mvc/VernacularConfig.cs
+++ Vernacular.Mvc/VernacularConfig.cs
@@ -11,10 +11,35 @@ namespace Vernacular.Mvc
 {
     public static class VernacularConfig
     {
+        static IRequestCultureProvider CultureProvider { get; set; }
         public static void Config(Catalog instance, IRequestCultureProvider cultureProvider)
         {
             Catalog.Implementation = instance;
-            IEnumerable<CultureInfo> cultureInfo = cultureProvider.GetCulture(new HttpContextWrapper(HttpContext.Current));
+            CultureProvider = cultureProvider;
+
+            HttpApplication app = HttpContext.Current.ApplicationInstance;
+            app.PreRequestHandlerExecute += app_PreRequestHandlerExecute;
+            app.Disposed += app_Disposed;
+        }
+
+        static void app_Disposed(object sender, EventArgs e)
+        {
+            HttpApplication app = sender as HttpApplication;
+            if (app != null)
+            {
+                app.PreRequestHandlerExecute -= app_PreRequestHandlerExecute;
+                app.Disposed -= app_Disposed;
+            }
+        }
+
+        static void app_PreRequestHandlerExecute(object sender, EventArgs e)
+        {
+            SetCultures();
+        }
+
+        public static void SetCultures()
+        {
+            IEnumerable<CultureInfo> cultureInfo = CultureProvider.GetCulture(new HttpContextWrapper(HttpContext.Current));
             Thread.CurrentThread.CurrentCulture = cultureInfo.First();
             Thread.CurrentThread.CurrentUICulture = cultureInfo.First();
         }
